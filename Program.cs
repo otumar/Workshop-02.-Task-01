b986817960bdb1c2478c9fb7d7afec68578fec20
«Тумар <otumar@yandex.ru>
2022-06-14 00:53:41 +0300

    Task01

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+System.Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+if (number.Length != 3) System.Console.WriteLine("Введено неверное число");
+else 
+{
+int number1 = numberInt % 100 / 10;
+System.Console.WriteLine ($"{number1} - Вторая цифра вашего числа");
+}
